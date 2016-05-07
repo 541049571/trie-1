f757b0c2c0d8e8229a39110e032999b8da176c9d
DerShokus <lily.coder@gmail.com>
2016-05-07 17:22:01 +0300

    fixes

--- include/trie.h
+++ include/trie.h
@@ -33,10 +33,10 @@ typedef void *(*trie_allocator_t)(size_t);
 typedef void (*trie_deallocator_t)(void *);
 
 /*
- * Create new trie object.
- * Returns trie object or null if operation failed.
- * If allocator is NULL - trie uses malloc.
- * If deallocator is NULL - trie uses free.
+ * Create a new trie object.
+ * Returns a trie object or null if the operation failed.
+ * If an allocator is NULL - trie uses malloc.
+ * If a deallocator is NULL - trie uses free.
  */
 struct trie *trie_new(trie_allocator_t allocator,
                       trie_deallocator_t deallocator);
@@ -47,30 +47,30 @@ struct trie *trie_new(trie_allocator_t allocator,
 void trie_delete(struct trie **trie);
 
 /*
- * Insert new data to the tree.
- * Previous data associated with the key will returned by old parameter and will
- * be replaced by data parameter.
+ * Insert new data into the trie.
+ * Previous data (associated with the key) will be returned by old parameter and
+ * will be replaced by data parameter.
  *
- * Returns true if operation completed successfull.
+ * Returns true if the operation completed successfully.
  */
 bool trie_insert(struct trie *root, const uint8_t *key, const size_t key_size,
                  void *data, void **old);
 
 /*
- * Get value associated with the key.
- * Value returns by data parameter.
+ * Get a value associated with the key.
+ * A value returns by data parameter.
  *
- * Returns true if key contains in the tree and the operation completed
- * successfull.
+ * Returns true if a trie contains the key and the operation completed
+ * successfully.
  */
 bool trie_at(struct trie *root, const uint8_t *key, const size_t key_size,
              void **data);
 
 /*
  * Remove subtree for the key.
- * The old value returns with data parameter.
+ * The old value returns by data parameter.
  *
- * Returns true if the operation completed successfull.
+ * Returns true if the operation completed successfully.
  */
 bool trie_remove(struct trie *obj, const uint8_t *key, const size_t key_size,
                  void **data);
